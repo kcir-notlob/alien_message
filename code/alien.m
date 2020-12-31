e8a0b6901cd0754c771e3a530ca694cdb87ec80e
kcir-notlob <kcir.notlob@gmail.com>
2020-12-31 13:54:11 +0000

    Update alien.m

--- code/alien.m
+++ code/alien.m
@@ -1,1 +1,12 @@
+#alien message decoding 
+#you may not need this if your setup is correct
+addpath("/home/test/octave/signal-1.4.1")
+name="unknown.wav";
 
+[x]=audioread(name);
+
+# matlab if we had spectrogram
+#spectrogram(x,'yaxis');
+
+#if you have octave
+specgram(x);
